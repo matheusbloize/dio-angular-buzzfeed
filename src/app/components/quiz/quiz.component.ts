52a0a5519828aa4f98865e39f8a2756ed5226d7b
gamdias sky <mbloizepd@gmail.com>
2022-11-21 17:04:27 -0300

    primeira fase de testes completa

---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quiz-questions.json'
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit {
+
+  title: string = ""
+
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answerSelected: string = ""
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerPick(value: string) {
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep() {
+    this.questionIndex += 1
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results]
+    }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(this.answers.length >= 10) {
+        this.answers.splice(0, 5)
+      }
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+        ){
+          return previous
+        } else {
+          return current
+        }
+      })
+    return result
+  }
+
+  returnHome(){
+    this.ngOnInit()
+    this.questions = quiz_questions.questions
+    this.questionSelected = this.questions[0]
+  }
+
+  returnQuestion(){
+    console.log(this.questionIndex)
+    if(this.questionIndex === 0) return
+    this.questionSelected = this.questions[this.questionIndex - 1]
+    this.questionIndex = this.questionIndex - 1
+  }
+}
